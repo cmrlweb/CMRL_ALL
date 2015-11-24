c78da897c220aaf4ed5528d81058db1125e8ed97
mohancool001 <mohanrajan1996@gmail.com>
2015-11-24 14:34:08 +0530

    24Nov

---
+++ Work File/webbackup 24nov/android_api/assets/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "cmrlweb");
+define("DB_PASSWORD", "MohanCEG15");
+define("DB_DB1", "cmrlmain");
+define("DB_DB2","eqmain1");
+?>
